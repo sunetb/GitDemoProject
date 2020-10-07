0094b95f1d9caa2701c0a72054c87392638fcb19
sunetb <sunetb@gmail.com>
2020-10-07 13:00:16 +0200

    Edited new comment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //a comment
+        //a very good comment
 	    //another comment
 	// write your code here
     }
